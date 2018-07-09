b3241b8af246e8bf06df8250a1b7a3650636f67a
Madeline Story <madeline1093@gmail.com>
2018-07-08 19:03:26 -0500

    testing monaco_fix file structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Monaco from "./components/Monaco";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to KettleCat</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div id="container">
+    <Monaco />
+  </div>
+);
+
 
 export default App;
---
+++ src/components/LanguageButton/LanguageButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Monaco from "../Monaco";
+
+const LanguageButton = () => {
+    <div className="dropdown">
+        
+    </div>
+}
+
+export default LanguageButton;
