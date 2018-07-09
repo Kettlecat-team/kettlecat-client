a177b2a455dedbd4318f2e24b72cf0c9d6e05652
Madeline Story <madeline1093@gmail.com>
2018-07-08 21:37:45 -0500

    connected graph ql

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 
 const client = new ApolloClient({
-  uri: "https://nx9zvp49q7.lp.gql.zone/graphql"
+  uri: "https://kettlecat-graphql.herokuapp.com/graphql"
 });
+
+const GET_CHAKIBOOS = gql`
+  query {
+    chakiboos {
+      id
+      title
+      description
+    }
+  }
+`;
 
 class Home extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <AppComponent />
+        <Query query={GET_CHAKIBOOS}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Loading...</div>;
+            if (error) return <div>Error :(</div>;
+            return <div>{data.chakiboos[0].title}</div>;
+          }}
+        </Query>
       </ApolloProvider>
     );
   }
