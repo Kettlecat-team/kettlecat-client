7a15be2eccee78cad6b5d4d40580c36ed767617f
Maxime Burlat <maximeburlat@gmail.com>
2018-07-17 07:17:16 -0500

    put a basic fetch to register a user, however, it will need more react router magic

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
+import { Button } from "../../../node_modules/@material-ui/core";
 
 const styles = theme => ({
   container: {
@@ -17,15 +18,47 @@ const styles = theme => ({
   }
 });
 
+const postData = (url = ``, data = {}) => {
+  // Default options are marked with *
+  return fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, cors, *same-origin
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+      // "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  })
+    .then(response => response.json()) // parses response to JSON
+    .catch(error => console.error(`Fetch Error =\n`, error));
+};
+
 class SignUp extends Component {
   state = {
-    username: ""
+    username: "",
+    password: "",
+    passwordConfirm: ""
   };
 
   handleChange = name => event => {
     this.setState({
       [name]: event.target.value
     });
+  };
+
+  handleSubmit = () => {
+    if (this.state.password === this.state.passwordConfirm) {
+      let newUser = {
+        username: this.state.username,
+        password: this.state.password
+      };
+      postData(`https://kettlecat-graphql.herokuapp.com/register`, newUser)
+        .then(data => console.log(data)) // JSON from `response.json()` call
+        .catch(error => console.error(error));
+    } else {
+      // TODO improve and maybe display a notification
+      console.log("passwords dont match");
+    }
   };
 
   render() {
@@ -44,6 +77,8 @@ class SignUp extends Component {
           id="password-input"
           label="Password"
           className={classes.textField}
+          value={this.state.password}
+          onChange={this.handleChange("password")}
           type="password"
           autoComplete="current-password"
           margin="normal"
@@ -52,10 +87,13 @@ class SignUp extends Component {
           id="password-confirmation"
           label="Password Confirmation"
           className={classes.textField}
+          value={this.state.passwordConfirm}
+          onChange={this.handleChange("passwordConfirm")}
           type="password"
           autoComplete="current-password"
           margin="normal"
         />
+        <Button onClick={this.handleSubmit}>Sign Up</Button>
       </form>
     );
   }
