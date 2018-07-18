e182e6fb1be4378c86590a24989ebee7672ddb04
robertas plynius <robertasplynius@yahoo.com>
2018-07-18 16:55:58 -0500

    added cards to chakiboolist

--- src/components/ChakibooList/ChakibooList.jsx
+++ src/components/ChakibooList/ChakibooList.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+
+
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
 const ChakibooList = (props) => {
 const {chakiboos} = props.data
-return(chakiboos.map(({title, description, id}) =>(
-    <Paper>
-        <div>{title} </div>
-        <div>{description} </div>
-    </Paper>
-)));
+const { classes } = props;
 
+  return ((chakiboos.map(({title, description, id}) =>(
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}> 
+            <div>{title} </div>
+            <div>{description} </div>
+         </Paper>
+        </Grid>
+      </Grid>
+    </div>
+    )))
+  );
 };
 
-export default ChakibooList;
+export default withStyles(styles)(ChakibooList);
