18d3fe1134192672a02252b695dcc4e99e549764
robertas plynius <robertasplynius@yahoo.com>
2018-07-25 19:22:22 -0500

    nothing added

--- src/components/ChakibooListContainer/ChakibooListContainer.jsx
+++ src/components/ChakibooListContainer/ChakibooListContainer.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Grid from '@material-ui/core/Grid';
 import ChakibooList from "../ChakibooList";
 
-
 const ChakibooListContainer = (props) => {
 
   return (
+
     <Grid container spacing={24}>
         <ChakibooList data={props} />
     </Grid>
+  
   )
 };
 
