f2dafeeee3bad77ac14eafa7e5804e3f8d6caae3
robertas plynius <robertasplynius@yahoo.com>
2018-07-25 20:44:04 -0500

    on click addded

--- src/components/ChakibooListContainer/ChakibooListContainer.jsx
+++ src/components/ChakibooListContainer/ChakibooListContainer.jsx
@@ -6,7 +6,6 @@ const ChakibooListContainer = (props) => {
 
   return (
 
-    <Grid container spacing={24}>
     <Grid container spacing={40} style={{ padding: 20, width: '100%' }}>
         <ChakibooList data={props} />
     </Grid>
